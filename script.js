437b3d8e5394eebb412aa2b04d1bc12e7445f41b
Arthur Mazzi <arthurmufjf@gmail.com>
2019-09-12 20:40:51 -0300

    funções de teclas

--- script.js
+++ script.js
@@ -5,7 +5,42 @@
 
     //Funções
 
-    var esq = 37, dir = 39, space = 32;
-    var 
+    //Variáveis
+    var esq = 37, dir = 39, enter = 13, space = 32;
+    var mesq = mdir = false;
 
+    //Estados
+    var loading = 0, jogando = 1, pause = 2, gg = 3;
+    var padrao = loading;
+
+    //Comandos de teclas
+    window.addEventListener('keydown', function(e){
+        var key = e.keyCode;
+        switch(key){
+            case esq:
+                mesq = true;
+                break;
+            case dir:
+                mdir = true;
+                break;
+        }
+                
+    }, false);
+    window.addEventListener('keyup', function(e){
+        var key = e.keyCode;
+        switch(key){
+            case esq:
+                mesq = false;
+                break;
+            case dir:
+                mdir = false;
+                break;
+            case enter:
+                if(padrao != jogando)
+                    padrao = jogando;
+                else
+                    padrao = pause;
+        }               
+    }, false);
 }())
+
